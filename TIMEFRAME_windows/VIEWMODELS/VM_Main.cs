7abeaa3db674234df671bb95bb9b71a0ea94d360
StijnDeVosDEV <sdv.foxdevelopments@gmail.com>
2020-02-06 22:26:18 +0100

    Implemented WPF project

---
+++ TIMEFRAME_windows/VIEWMODELS/VM_Main.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TIMEFRAME_windows.SERVICES;
+
+namespace TIMEFRAME_windows.VIEWMODELS
+{
+    public class VM_Main : Base.GEN_ObservableObject
+    {
+        //  -----------------------------
+        //  Private variable declarations
+        //  -----------------------------
+        // UI related
+
+
+        // Commands
+        
+
+        // Services
+
+
+        //  -----------------------------
+        //  Actual Class variables
+        //  -----------------------------
+
+
+        //  -----------
+        //  CONSTRUCTOR
+        //  -----------
+        public VM_Main()
+        {
+            // Initializations
+
+
+            // Load commands
+            LoadCommands();
+
+            // Initialize process
+            Logger.SetPath(Environment.GetEnvironmentVariable("TEMP"));
+            Logger.Initialize();
+            
+            // Search Background Worker event handler initialization
+            
+        }
+
+        // ----------------------------
+        // Public variable declarations
+        // ----------------------------
+        #region PROPERTIES
+
+        #endregion
+
+
+        // ---------------------------
+        // Public command declarations
+        // ---------------------------
+        #region COMMANDS
+
+        #endregion
+
+
+        //  -----------------
+        //  Private functions
+        //  -----------------
+
+        //  ----------------------
+        // COMMAND RELATED METHODS
+        //  ----------------------
+        /// <summary>
+        /// Load all GUI commands
+        /// </summary>
+        private void LoadCommands()
+        {
+            
+        }
+    }
+}
--- TIMEFRAME_windows/VIEWS/V_Main.xaml.cs
+++ TIMEFRAME_windows/VIEWS/V_Main.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -10,17 +8,16 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TIMEFRAME_windows
+namespace TIMEFRAME_windows.VIEWS
 {
     /// <summary>
-    /// Interaction logic for MainWindow.xaml
+    /// Interaction logic for V_Main.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class V_Main : Window
     {
-        public MainWindow()
+        public V_Main()
         {
             InitializeComponent();
         }
