b6f9c95a080971a869419d48160161260671ddf2
StijnDeVosSISW <stijn.de_vos@siemens.com>
2020-02-07 12:16:54 +0100

    Implemented part of content viewpart

--- TIMEFRAME_windows/VIEWS/V_Main.xaml.cs
+++ TIMEFRAME_windows/VIEWS/V_Main.xaml.cs
@@ -17,9 +17,73 @@ namespace TIMEFRAME_windows.VIEWS
     /// </summary>
     public partial class V_Main : Window
     {
+        // Local variables
+        private static BrushConverter bc = new BrushConverter();
+        private Brush highlightColor_SecondMenu = (Brush)bc.ConvertFrom("#FF3FC1C9");
+        private Brush highlightColor_Transparent = new SolidColorBrush(Colors.Transparent);
+
+        private static double OrigHeight = 0.0;
+
+        // CONSTRUCTOR
         public V_Main()
         {
             InitializeComponent();
+        }
+
+        private void StackPanel_Config_Customer_MouseEnter(object sender, MouseEventArgs e)
+        {
+            StackPanel_Config_Customer.Background = highlightColor_SecondMenu;
+        }
+
+        private void StackPanel_Config_Customer_MouseLeave(object sender, MouseEventArgs e)
+        {
+            StackPanel_Config_Customer.Background = highlightColor_Transparent;
+        }
+
+        private void StackPanel_Config_Project_MouseEnter(object sender, MouseEventArgs e)
+        {
+            StackPanel_Config_Project.Background = highlightColor_SecondMenu;
+        }
+
+        private void StackPanel_Config_Project_MouseLeave(object sender, MouseEventArgs e)
+        {
+            StackPanel_Config_Project.Background = highlightColor_Transparent;
+        }
+
+        private void StackPanel_Config_Task_MouseEnter(object sender, MouseEventArgs e)
+        {
+            StackPanel_Config_Task.Background = highlightColor_SecondMenu;
+        }
+
+        private void StackPanel_Config_Task_MouseLeave(object sender, MouseEventArgs e)
+        {
+            StackPanel_Config_Task.Background = highlightColor_Transparent;
+        }
+
+        private void StackPanel_Config_TimeEntry_MouseEnter(object sender, MouseEventArgs e)
+        {
+            StackPanel_Config_TimeEntry.Background = highlightColor_SecondMenu;
+        }
+
+        private void StackPanel_Config_TimeEntry_MouseLeave(object sender, MouseEventArgs e)
+        {
+            StackPanel_Config_TimeEntry.Background = highlightColor_Transparent;
+        }
+
+        private void Img_Expand2_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            if (Grid_Content.Height > 0)
+            {
+                OrigHeight = Grid_Content.Height;
+                Grid_Content.Height = 0;
+                
+                //Img_Expand2.Source = new BitmapImage(new Uri("pack://application:,,,/TIMEFRAME_windows;component/IMAGES/SortArrowDOWN_Black.png"));
+            }
+            else
+            {
+                Grid_Content.Height = OrigHeight;
+                //Img_Expand2.Source = new BitmapImage(new Uri("pack://application:,,,/TIMEFRAME_windows;component/IMAGES/SortArrowUP_Black.png"));
+            }
         }
     }
 }
